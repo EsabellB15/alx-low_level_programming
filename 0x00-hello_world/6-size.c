332a33c4323bb34f515800713a029f9acf15ebd5
EsabellB15 <esabellab7@gmail.com>
2023-02-16 15:43:41 -0800

    Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /**
  * main - Prints the size of various types based on 
  * the computer it is compiled and run on..
@@ -8,7 +7,6 @@
  */
 
 int main(void)
-
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
