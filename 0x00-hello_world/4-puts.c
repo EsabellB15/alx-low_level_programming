de563f29fc02fddcb998755553ea41f2f733885d
EsabellB15 <esabellab7@gmail.com>
2023-02-16 14:44:42 -0800

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,16 +2,14 @@
 
 
 /**
- * main-prints "Programming is like building a multilingual 
- * puzzle, followed by a newline.
+ * main - prints "Programming is like building a multilingual
+ * puzzle, follwed by a new line.
  * Return: Always 0.
  */
-
 
 int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
