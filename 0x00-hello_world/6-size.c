5f2e4fbc6cd3ad725a9c36a4040ae3afb3584b5f
EsabellB15 <esabellab7@gmail.com>
2023-02-16 16:58:01 -0800

    6 size c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Prints the size of various types based on 
- * the computer it is compiled and run on.
- * Return: Always 0.
+ * main - main function 
+ *
+ * Return: o on success
  */
 
 int main(void)
