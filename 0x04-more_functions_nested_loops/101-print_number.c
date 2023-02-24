f57754f86065ab06a3ff81081770008edb8f7b48
EsabellB15 <esabellab7@gmail.com>
2023-02-24 13:56:00 -0800

    Numbers have life

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,7 +2,7 @@
 #include  <stdio.h>
 
 /**
-* print_number  - Prints an integer
+* print_number  - Prints an integer.
 * @n: The integer to prints.
 * Returns: Nothing!
 */
@@ -12,9 +12,9 @@ void print_number(int n)
 {
 	unsigned int k = n;
 
-	if (n < 0 )
+	if (n < 0)
 	{
-	n *= - 1;
+	n *= -1;
 	k = n;
 	_putchar('–');
 	}
