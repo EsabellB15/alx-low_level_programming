f5ad4401f0cb155f477c474a91ad0be791abf977
EsabellB15 <esabellab7@gmail.com>
2023-03-02 07:57:34 -0800

    strcat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,17 +3,17 @@
 
 #include <stdio.h>
 
-Char  *_strcat(char *dest, char *src);
-Char *_strncat(char *dest, char *src, int n);
-Char *_strncpy(char *dest, char *src, int n);
-Int _strcmp(char *s1,char *sr);
-Void reverse_array(int *a, int n);
-Char *string_toupper(char *);
-Char *cap_string(char *);
-Char *leet(char *);
-Char *rot13(char *);
-Void print_number(int n);
-Char *infinite_add(char *n1, char *n2, char *r, int size_r);
-Void print_buffer(char *b, int size);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1,char *sr);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 #endif
 
