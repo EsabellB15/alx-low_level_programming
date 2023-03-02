fb79a774fbc9be71be5b11fca5098fecb9388370
EsabellB15 <esabellab7@gmail.com>
2023-03-02 09:27:32 -0800

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,11 +16,12 @@ char *_strncpy(char *dest, char *src, int n)
 	j = 0;
 	while (j < n && src[j] != '\0')
 	{
+	dest[j] = src[j];
 	j++;
 	}
 	while (j < n)
 	{
-	dest[i] = '\0';
+	dest[j] = '\0';
 	j++;
 	}
 	return (dest); 
