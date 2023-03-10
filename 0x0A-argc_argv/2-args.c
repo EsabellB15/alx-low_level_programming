04d981a0267d7adcd838a6c0547f3e514ec6fca5
EsabellB15 <esabellab7@gmail.com>
2023-03-10 01:54:11 -0800

    mul

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -12,15 +12,14 @@
 int main(int argc, char *argv[])                                                                                                     
 {                                                                                                                                    
                                                                                                                                      
-/*Declaring variables*/                                                                                                              
-int count = 0;                                                                                                                       
-                                                                                                                                     
-if (argc > 0)                                                                                                                        
-{                                                                                                                                    
-/*WHILE - Print each arguments*/                                                                                                     
-while (count < argc)                                                                                                                 
-{                                                                                                                                    
-printf("%s\n", argv[count]);
-}
+	/*Declaring variables*/                                                                                                              
+	int count = 0;                                                                                                                                                                                                                                             
+	if (argc > 0)                                                                                                                       
+	{                                                                                                                                    
+	/*WHILE - Print each arguments*/                                                                                                     
+	while (count < argc)                                                                                                                 
+	{                                                                                                                                    
+	printf("%s\n", argv[count]);
+	}
 }
 }
